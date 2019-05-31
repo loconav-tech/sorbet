0afe6defa7aaa2937b23efa56ddd0234091c7059
Dmitry Petrashko <dmitry@stripe.com>
2019-05-31 13:40:35 -0700

    Update

--- .buildkite/tools/setup-linux.sh
+++ .buildkite/tools/setup-linux.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 
 echo "--- installing dependencies"
+apt-get update
 apt-get install -yy pkg-config zip g++ zlib1g-dev unzip python ruby autoconf git
 
 if [[ -n "${CLEAN_BUILD-}" ]]; then
