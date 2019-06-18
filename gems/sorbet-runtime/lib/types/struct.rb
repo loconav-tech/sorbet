e2671125ceedd9c214879f75bc1ee1bb71678307
Ariel Davis <azdavis@stripe.com>
2019-06-18 15:38:46 -0700

    Add DeprecatedInheritableStruct to runtime

--- gems/sorbet-runtime/lib/types/struct.rb
+++ gems/sorbet-runtime/lib/types/struct.rb
@@ -1,8 +1,18 @@
 # frozen_string_literal: true
 # typed: true
 
-class T::Struct
+class T::DeprecatedInheritableStruct
   include T::Props
   include T::Props::Serializable
   include T::Props::Constructor
+end
+
+class T::Struct < T::DeprecatedInheritableStruct
+  def self.inherited(subclass)
+    super(subclass)
+    T::Private::ClassUtils.replace_method(subclass.singleton_class, :inherited) do |s|
+      super(s)
+      raise "#{self.name} is a subclass of T::Struct and cannot be subclassed"
+    end
+  end
 end
